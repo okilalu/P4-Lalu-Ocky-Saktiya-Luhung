09a41195bc5bf68b10addf476fb296c92687c7c5
okilalu <65159706+okilalu@users.noreply.github.com>
2020-05-12 20:06:46 +0800

    Create Jobsheet 1 C++.cpp
    Program array satu dimensi menampilkan NIM

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+main(){
+	cout<<"Nama : LALU OCKY SAKTIYA LUHUNG "<<endl;
+	cout<<"NIM  : F1B019079"<<endl;
+	cout<<"Kelompok : 16 "<<endl<<endl;
+	
+	int Nim_Mhsw [9]={5,6,7,8,10,12,14,16,18};
+	cout<<("Nim ke-1 = ", Nim_Mhsw[0])<<endl;
+	cout<<("Nim ke-2 = ", Nim_Mhsw[1])<<endl;
+	cout<<("Nim ke-3 = ", Nim_Mhsw[2])<<endl;
+	cout<<("Nim ke-4 = ", Nim_Mhsw[3])<<endl;
+	cout<<("Nim ke-5 = ", Nim_Mhsw[4])<<endl;
+	cout<<("Nim ke-6 = ", Nim_Mhsw[5])<<endl;
+	cout<<("Nim ke-7 = ", Nim_Mhsw[6])<<endl;
+	cout<<("Nim ke-8 = ", Nim_Mhsw[7])<<endl;
+	cout<<("Nim ke-9 = ", Nim_Mhsw[8])<<endl;
+}
