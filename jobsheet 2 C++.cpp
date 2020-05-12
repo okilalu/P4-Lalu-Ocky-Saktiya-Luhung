bd2fc3cd3146dde2aae4338652c9ff4c894f609c
okilalu <65159706+okilalu@users.noreply.github.com>
2020-05-12 20:08:58 +0800

    Create jobsheet 2 C++.cpp
    Mengisi dan menampilkan elemen array index berdasarkan NIM

---
+++ jobsheet 2 C++.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	cout<<"Nama     : LALU OCKY SAKTIYA LUHUNG"<<endl;
+	cout<<"Nim      : F1B019079"<<endl;
+	cout<<"Kelompok : 16"<<endl;
+	
+	int a [9];
+	
+	cout<<"Input Nilai ke-1 : ";
+	cin>>a[0];
+	
+	cout<<"Input Nilai ke-2 : ";
+	cin>>a[1];
+	
+	cout<<"Input Nilai ke-3 : ";
+	cin>>a[2];
+	
+	cout<<"Input Nilai ke-4 : ";
+	cin>>a[3];
+	
+	cout<<"Input Nilai ke-5 : ";
+	cin>>a[4];
+	
+	cout<<"Input Nilai ke-6 : ";
+	cin>>a[5];		
+	
+	cout<<"Input Nilai ke-7 : ";
+	cin>>a[6];		
+	
+	cout<<"Input Nilai ke-8 : ";
+	cin>>a[7];
+	
+	cout<<"Input Nilai ke-9 : ";
+	cin>>a[8];				
+}
